a764474248f190380b8093c0d2d790705e3c7db8
Tomasz Kacprzak <monkhtc@gmail.com>
2018-05-02 13:45:07 +0200

    rrsp79 v.1.01

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,34 @@
 
 
-	var newsButton = document.getElementById('news');
-	var crewButton = document.getElementById('crew');
-	var projectsButton = document.getElementById('projects');
-	var fundsButton = document.getElementById('funds');
-	var insuranceButton = document.getElementById('insurance');
-	var downloadButton = document.getElementById('download');
-	var contactButton = document.getElementById('contact');
-	var showBackground = document.getElementById('contentbackground');
-	var showVerticalMenu = document.getElementById('verticalmenu');
-	
-		function showBackgroundContent() {
-			showBackground.style.left = '10%';
-			showVerticalMenu.style.left = '0';
+	var horizontalMenu = document.getElementById('horizontalmenu');
+	var frontButtons = document.getElementsByClassName("btn");
+	var menuIconBtn = document.getElementsByClassName("menuicon");
+	var contentSection = document.getElementById('contentbackground');
+
+		function showHorizontalMenu() {
+			horizontalMenu.style.top = '20vh';
 		}
 
-		newsButton.addEventListener('click', showBackgroundContent);
-		crewButton.addEventListener('click', showBackgroundContent);
-		projectsButton.addEventListener('click', showBackgroundContent);
-		fundsButton.addEventListener('click', showBackgroundContent);
-		insuranceButton.addEventListener('click', showBackgroundContent);
-		downloadButton.addEventListener('click', showBackgroundContent);
-		contactButton.addEventListener('click', showBackgroundContent);
+			for (var i = 0 ; i < frontButtons.length ; i++) {
+		 	frontButtons[i].addEventListener('click' , showHorizontalMenu );
+			}
 
+		function hideMenu() {
+			var menu = document.getElementById('menu');
+			var mainmenu = document.getElementById('mainmenu');
 
+			menu.classList.add('fadeout');
+
+			setTimeout(function() {
+				menu.style.display = '';
+				menu.style.display = 'none';
+				mainmenu.style.margin = '';
+				mainmenu.style.margin = '0';
+			}, 400);
+
+		}
+
+			for (var i = 0 ; i < frontButtons.length ; i++) {
+		 	frontButtons[i].addEventListener('click' , hideMenu );
+			}
+			
