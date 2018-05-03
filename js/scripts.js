d42dca3f6b767ff5f3d811980d77829adbd2de0e
Tomasz Kacprzak <monkhtc@gmail.com>
2018-05-03 20:21:56 +0200

    rrsp79 v.1.02

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,12 @@
 	var horizontalMenu = document.getElementById('horizontalmenu');
 	var frontButtons = document.getElementsByClassName("btn");
 	var menuIconBtn = document.getElementsByClassName("menuicon");
-	var contentSection = document.getElementById('contentbackground');
+	var contentCrew = document.getElementById('contentcrew');
+	var contentFunds = document.getElementById('contentfunds');
+	var crewHorizontalMenuBtn = document.getElementById('menuiconcrew');
+	var fundsHorizontalMenuBtn = document.getElementById('menuiconfunds');
+	var crewBtnClick = crewHorizontalMenuBtn.addEventListener('click' , crewBtnClicked);
+	var fundsBtnClick = fundsHorizontalMenuBtn.addEventListener('click' , fundsBtnClicked);
 
 		function showHorizontalMenu() {
 			horizontalMenu.style.top = '20vh';
@@ -16,7 +21,7 @@
 		function hideMenu() {
 			var menu = document.getElementById('menu');
 			var mainmenu = document.getElementById('mainmenu');
-
+			
 			menu.classList.add('fadeout');
 
 			setTimeout(function() {
@@ -24,11 +29,33 @@
 				menu.style.display = 'none';
 				mainmenu.style.margin = '';
 				mainmenu.style.margin = '0';
-			}, 400);
-
+			}, 300);
 		}
 
 			for (var i = 0 ; i < frontButtons.length ; i++) {
-		 	frontButtons[i].addEventListener('click' , hideMenu );
+		 		frontButtons[i].addEventListener('click' , hideMenu );
+
 			}
+
+			// Horizontal Menu Menagment
+		
 			
+
+			function crewBtnClicked() {
+				contentFunds.style.display = ''
+				contentFunds.style.display = 'none';
+				contentCrew.style.display = '';
+				contentCrew.style.display = 'flex';					
+			}
+			contentCrew.classList.add('fadein');
+			crewBtnClick;
+
+			function fundsBtnClicked() {
+				contentCrew.style.display = ''
+				contentCrew.style.display = 'none';
+				contentFunds.style.display = '';
+				contentFunds.style.display = 'flex';					
+			}
+
+			contentFunds.classList.add('fadein');
+			fundsBtnClick;
